dfa0c3eb2b9b6a1d0ccfb84ce0fbc629024d5577
Raheel Ahmed <101556343+raheel-436@users.noreply.github.com>
2024-08-24 22:00:41 +0500

    'faq icon changed based on selection'

--- scripts/script.js
+++ scripts/script.js
@@ -6,15 +6,20 @@ faqAnswers.forEach((answer) => {
   answer.style.display = "none";
 });
 
-faqTitles.forEach((faqTitle) => {
+faqTitles.forEach((faqTitle, index) => {
   let selected = false;
   faqTitle.addEventListener("click", () => {
     const currentAnswer = faqTitle.nextElementSibling;
+    const currentIcon = faqIcons[index];
     if (!selected) {
       currentAnswer.style.display = "block";
+      currentIcon.style.backgroundImage =
+        "url('../assets/images/icon-minus.svg')";
       selected = true;
     } else {
       currentAnswer.style.display = "none";
+      currentIcon.style.backgroundImage =
+        "url('../assets/images/icon-plus.svg')";
       selected = false;
     }
   });
